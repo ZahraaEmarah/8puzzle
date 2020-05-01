e4493160ac00e7ed40acb4ec17e1dc43090b7c14
Zahraa Emarah <emarahzahraa@gmail.com>
2020-05-01 04:04:03 +0200

    Add files via upload

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -5,7 +5,7 @@ import time
 
 
 class Node:
-
+    # x and y axis
     x = [0, 1, 2, 0, 1, 2, 0, 1, 2]
     y = [2, 2, 2, 1, 1, 1, 0, 0, 0]
 
@@ -48,33 +48,30 @@ class Node:
         new[index + 3] = temp_state
         return new
 
-    def __lt__(self, other):
+    def __lt__(self, other):  # sort the heap on the cost of states in A* algorithm
         return self.cost < other.cost
 
-    def __eq__(self, other):
+    def __eq__(self, other):  # compare nodes on their states only
         if not isinstance(other, Node):
             return NotImplemented
         return self.state == other.state
 
 
 # algorithm for DFS
-
-
 def dfs(initial_state, goal_state):
     steps = []  # list to trace back the steps
     frontier = []  # initialize the stack
     explored = set()  # the explored is an empty set
-    int_state = Node(initial_state, None)
+    fexplored = set()  # used to combine the frontier and explored
+    int_state = Node(initial_state, None)  # intial state of the board
     nodes_expanded = 0
     depth = 0
-    frontier.append(int_state)
+    frontier.append(int_state)  # append the intial state to the frontier
     while frontier:  # loop till the stack is empty
-        if nodes_expanded == 1000:
-            print("infinite loop")
-            return "FAILURE"
-        popped_state = frontier.pop()
-        explored.add(tuple(popped_state.state))
-        printboard(popped_state.state)
+        popped_state = frontier.pop()  # pop the top board found in the frontier
+        explored.add(tuple(popped_state.state))  # add it to the explored
+        fexplored.add(tuple(popped_state.state))
+        printboard(popped_state.state)  # print it
         if np.array_equal(goal_state, popped_state.state):
             print("SUCCESS")
             print("******************")
@@ -82,10 +79,10 @@ def dfs(initial_state, goal_state):
             print("Path to goal:")
             parent = popped_state.parent  # get the current state's parent
             steps.append(popped_state)
-            while parent:       # get the parent of each node until we reach the root
+            while parent:  # get the parent of each node until we reach the root
                 steps.append(parent)
                 parent = parent.parent
-                depth = depth + 1
+                depth = depth + 1  # to calculate the depth
             while steps:  # print the trace of steps from the start
                 x = steps.pop()
                 printboard(x.state)
@@ -93,11 +90,11 @@ def dfs(initial_state, goal_state):
             print("Total nodes expanded =", nodes_expanded)
             return "SUCCESS"
         nodes_expanded = nodes_expanded + 1
-        neighbors = children(popped_state)
-        for neighbor in neighbors:
-            if tuple(neighbor.state) not in explored:
+        neighbors = children(popped_state)  # get the children of this node
+        for neighbor in neighbors:  # loop on this node to add them to the frontier
+            if tuple(neighbor.state) not in fexplored:  # if not in frontier or explored
                 frontier.append(neighbor)
-                explored.add(tuple(neighbor.state))
+                fexplored.add(tuple(neighbor.state))  # this is used to add both explored and frontier
     return "FAILURE"
 
 
@@ -120,7 +117,7 @@ def bfs(initial_state, goal_test):
             print("Path to goal: ")
             parent = state.parent  # get the current state's parent
             steps.append(state)
-            while parent:       # get the parent of each node until we reach the root
+            while parent:  # get the parent of each node until we reach the root
                 steps.append(parent)
                 parent = parent.parent
                 depth = depth + 1
@@ -140,39 +137,35 @@ def bfs(initial_state, goal_test):
 
 def a_star(initial_state, goal_state, heuristic):
     print("\n**** " + heuristic.upper() + " DISTANCE ****")
+
+    # initialize root
     g = 0
     depth = 0
-    expanded = 1
     cost_of_path = 0
-
     int_state = Node(initial_state, None)
     goal = Node(goal_state, None)
+    expanded = 1
 
     if heuristic == "manhattan":
-        int_state.h = manhattan_distance(int_state, goal)
-        int_state.g = g
-        int_state.cost = int_state.g + int_state.h
+        int_state.h = manhattan_distance(int_state, goal)  # get h(n) of root
+        int_state.g = g  # initialize g(n)
+        int_state.cost = int_state.g + int_state.h  # calculate cost of root
     elif heuristic == "euclidean":
         int_state.h = euclidean_distance(int_state, goal)
         int_state.g = g
         int_state.cost = int_state.g + int_state.h
 
-    frontier = [int_state]
-    explored = []
-    steps = []
+    frontier = [int_state]  # initialize a Min heap
+    explored = []  # list of explored states
+    steps = []  # list of states of the final path (steps to solution)
 
-    while frontier:
-        state = heapq.heappop(frontier)
+    while frontier:  # while the heap is not empty
+        state = heapq.heappop(frontier)  # pop the state with the minimum cost
         heapq.heapify(frontier)
-        # print("\n")
-        # print("g = {}, h = {}".format(state.g, state.h))
-        # print("cost is {}".format(state.cost))
-        # print("expanded states = {}".format(expanded))
-        # printboard(state.state)
-        neighbors = children(state)
-        explored.append(state)
+        children_of_poppedstate = children(state)  # get all possible moves from the  popped state
+        explored.append(state)  # add it to the list of explored states
 
-        if np.array_equal(goal_state, state.state):
+        if np.array_equal(goal_state, state.state):  # if the popped state is the goal state
             print("SUCCESS")
             print("******************")
             print("Steps of solution:")
@@ -184,53 +177,60 @@ def a_star(initial_state, goal_state, heuristic):
             while steps:  # print the trace of steps from the start
                 z = steps.pop()
                 depth = z.g
-                print("g = {}, h = {}".format(z.g, z.h))
-                print("cost is {}".format(z.cost))
+                print("g = {}, h = {}".format(z.g, round(z.h, 2)))
+                print("cost is {}".format(round(z.cost, 2)))
                 cost_of_path = cost_of_path + z.cost
                 printboard(z.state)
             print("Depth = {}".format(depth))
             print("cost of path = {}".format(cost_of_path))
-            print("Expanded nodes of {} distance = {}".format(heuristic, len(explored)))
+            print("Expanded nodes of {} distance = {}".format(heuristic, expanded))
             return "SUCCESS"
 
         expanded = expanded + 1
-
-        for x in neighbors:
+        # if we reach this point it means the popped state isn't the goal state
+        # we loop on all its possible children
+        for x in children_of_poppedstate:
 
             if heuristic == "manhattan":
-                x.h = manhattan_distance(x, goal)
-                x.g = x.parent.g + 1
-                x.cost = x.g + x.h
+                x.h = manhattan_distance(x, goal)  # calculate manhattan heuristic
+                x.g = x.parent.g + 1  # set g(n)
+                x.cost = x.g + x.h  # calculate cost
             elif heuristic == "euclidean":
-                x.h = euclidean_distance(x, goal)
+                x.h = euclidean_distance(x, goal)  # calculate euclidean heuristic
                 x.g = x.parent.g + 1
                 x.cost = x.g + x.h
 
+            # if child is a new state
             if x not in explored and x not in frontier:
-                heapq.heappush(frontier, x)
+                heapq.heappush(frontier, x)  # add it to the heap of unexplored states
                 heapq.heapify(frontier)
 
+            # if child has been explored before
             if x in explored:
                 index = get_index(explored, x)
-                if explored[index].cost > x.cost:
-                    explored.remove(x)
-                    heapq.heappush(frontier, x)
+                # compare its cost with its version in the explored list
+                if explored[index].cost > x.cost:  # if it has a better cost than its version
+                    explored.remove(x)  # remove the version
+                    heapq.heappush(frontier, x)  # insert the new state with the better cost
                     heapq.heapify(frontier)
 
+            # if child in the heap of states to be explored
             if x in frontier:
                 index = get_index(frontier, x)
-                if frontier[index].cost > x.cost:
-                    frontier[index].cost = -100
-                    heapq.heappop(frontier)
+                # compare its cost with its version in the heap
+                if frontier[index].cost > x.cost:  # if it has a better cost than its version
+                    frontier[index].cost = -100  # decrease the key of its version
+                    heapq.heappop(frontier)  # remove it from the heap
                     heapq.heapify(frontier)
-                    heapq.heappush(frontier, x)
+                    heapq.heappush(frontier, x)  # insert the new state with the better cost
                     heapq.heapify(frontier)
 
     return "FAILURE"
 
 
+# function to get the index of a state in a list
 def get_index(heap, node):
-    for i in range(len(heap)+1):
+    for i in range(len(heap) + 1):
         if node.state == heap[i].state:
             return i
 
@@ -267,31 +267,35 @@ def move(state, the_move, node):
 
 def manhattan_distance(current_state, goal_state):
     total_h = 0
-    c_state = list(current_state.state)
-    g_state = list(goal_state.state)
+    c_state = list(current_state.state)  # current state
+    g_state = list(goal_state.state)  # goal state
 
-    for i in current_state.state:
-        if i == 0:
+    for i in current_state.state:  # loop on all numbers in a state
+        if i == 0:  # zero isn't included in the calculation of manhattan heuristic
             continue
-        index_current = c_state.index(i)
-        index_goal = g_state.index(i)
+        index_current = c_state.index(i)  # get the number's index in the current state
+        index_goal = g_state.index(i)  # get the number's index in the goal state
+        # calculate hn(n)
         h = abs(current_state.x[index_current] - goal_state.x[index_goal]) \
             + abs(current_state.y[index_current] - goal_state.y[index_goal])
-        total_h = total_h + h
+        total_h = total_h + h  # h(n) total
     return total_h
 
 
 def euclidean_distance(current_state, goal_state):
     total_h = 0
-    c_state = list(current_state.state)
-    g_state = list(goal_state.state)
+    c_state = list(current_state.state)  # current state
+    g_state = list(goal_state.state)  # goal state
 
-    for i in current_state.state:
-        index_current = c_state.index(i)
-        index_goal = g_state.index(i)
-        h = math.sqrt(pow((current_state.x[index_current] - goal_state.x[index_goal]), 2)) \
-            + math.sqrt(pow((current_state.y[index_current] - goal_state.y[index_goal]), 2))
-        total_h = total_h + h
+    for i in current_state.state:  # loop on all numbers in a state
+        if i == 0:  # zero isn't included in the calculation of the heuristic
+            continue
+        index_current = c_state.index(i)  # get the number's index in the current state
+        index_goal = g_state.index(i)  # get the number's index in the goal state
+        # calculate hn(n)
+        h = math.sqrt(pow((current_state.x[index_current] - goal_state.x[index_goal]), 2) + pow(
+            (current_state.y[index_current] - goal_state.y[index_goal]), 2))
+        total_h = total_h + h  # h(n) total
     return total_h
 
 
@@ -304,7 +308,9 @@ def main():
     goal_state = 0, 1, 2, 3, 4, 5, 6, 7, 8
     # initial_state = eval(sys.argv[1])
     # algorithm = sys.argv[2]
-    initial_state = 2, 3, 0, 1, 6, 4, 5, 7, 8
+    # initial_state = 1, 2, 5, 3, 4, 0, 6, 7, 8
+    # initial_state = 3, 1, 2, 4, 0, 5, 6, 7, 8
+    initial_state = 2, 8, 0, 6, 1, 5, 3, 7, 4
     algorithm = "a*"
     if initial_state.__len__() != 9:
         print("Error: Wrong number of entries")
